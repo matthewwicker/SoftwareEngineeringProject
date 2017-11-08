8847cfa99ccd3c584ad40a724bb29694fb13ea15
matthewwicker <mrw64879@uga.edu>
2017-11-08 18:37:10 -0500

    Added book access layer

---
+++ ThomasEclipse/Books/src/DatabaseAccess/BookDBManager.java
@@ -0,0 +1,22 @@
+package DatabaseAccess;
+import Entities.Book;
+public class BookDBManager {
+	/**
+	 * add book to database
+	 * @return -1 if failure and 1 if success
+	 */
+	public static int addBook() {
+		return -1;
+	}
+	/**
+	 * get book from database
+	 * @return -1 if failure and 1 if success
+	 */
+	public static int removeBook() {
+		return -1;
+	}
+	public static Book[] searchBooks(){
+		Book[] search_results = null;
+		return search_results;
+	}
+}
