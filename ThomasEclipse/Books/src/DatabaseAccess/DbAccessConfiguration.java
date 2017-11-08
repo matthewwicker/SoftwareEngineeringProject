7cac1caba96201e9a1c682e7e71286c9a0c975f2
Shadowarc72 <31143781+Shadowarc72@users.noreply.github.com>
2017-11-08 17:18:54 -0500

    Pushing the Project to My Branch
    I am adding the project to my branch just in case so I don't ruin everything

---
+++ ThomasEclipse/Books/src/DatabaseAccess/DbAccessConfiguration.java
@@ -0,0 +1,12 @@
+package DatabaseAccess;
+
+public abstract class DbAccessConfiguration {
+
+	protected String DB_DRIVE_NAME = "com.mysql.jdbc.Driver";
+	
+	protected String DB_CONNECTION_URL = "jdbc:mysql://localhost:3306/softwareengineeringproject";
+	
+	protected String DB_CONNECTION_USERNAME = "root";
+	
+	protected String DB_CONNECTION_PASSWORD = "1234";
+}
