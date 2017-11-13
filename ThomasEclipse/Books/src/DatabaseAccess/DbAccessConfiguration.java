21eab1f2a862114dac6bd5c18b2160edf5e910da
snelliott <sarah.elliott26@uga.edu>
2017-11-13 11:22:51 -0500

    servlet has Task and Navigator

--- ThomasEclipse/Books/src/DatabaseAccess/DbAccessConfiguration.java
+++ ThomasEclipse/Books/src/DatabaseAccess/DbAccessConfiguration.java
@@ -4,9 +4,9 @@ public abstract class DbAccessConfiguration {
 
 	protected String DB_DRIVE_NAME = "com.mysql.jdbc.Driver";
 	
-	protected String DB_CONNECTION_URL = "jdbc:mysql://localhost:3306/Books";
+	protected String DB_CONNECTION_URL = "jdbc:mysql://localhost:3306/bookz";
 	
 	protected String DB_CONNECTION_USERNAME = "root";
 	
-	protected String DB_CONNECTION_PASSWORD = "password";
+	protected String DB_CONNECTION_PASSWORD = "cho";
 }
