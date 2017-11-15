52c7a1ec8edaa2a90e1927d7976be162b9f92415
Shadowarc72 <31143781+Shadowarc72@users.noreply.github.com>
2017-11-15 17:28:05 -0500

    Fixing merge conflicts
    still making changes to address accesser

---
+++ ThomasEclipse/Books/src/DatabaseAccess/AddressDBManager.java
@@ -0,0 +1,10 @@
+package DatabaseAccess;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import Entities.User;
+public class AddressDBManager {
+	private static Driver driver = new Driver();
+	
+}
