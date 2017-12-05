f02746f07922176b9bae40bf1409305866a478c6
snelliott <sarah.elliott26@uga.edu>
2017-12-05 03:48:29 -0500

    for heavens sake

--- ThomasEclipse/Books/src/View/myservlet.java
+++ ThomasEclipse/Books/src/View/myservlet.java
@@ -1,12 +1,5 @@
 package View;
-import DatabaseAccess.Driver;
-import DatabaseAccess.PromoDBManager;
-import DatabaseAccess.SupplierDBManager;
-import DatabaseAccess.TransactionDBManager;
-import DatabaseAccess.BookDBManager;
-import DatabaseAccess.CartDBManager;
-import DatabaseAccess.CartItemDBManager;
-import DatabaseAccess.UserDBManager;
+import DatabaseAccess.*;
 import EmailNotifications.SendEmail;
 
 import java.io.IOException;
@@ -49,6 +42,7 @@ public class myservlet extends HttpServlet {
     private Promo userPromotion = new Promo();
 	Book newBook = new Book();
 	User newUser = new User();
+	Payment pay  = new Payment();
 	private static final long serialVersionUID = 1L;
     Configuration cfg = null;
     private String templateDir = "/WEB-INF/templates";
@@ -72,7 +66,6 @@ public class myservlet extends HttpServlet {
         root.put("promocode", "");
         root.put("message", "");
         root.put("error", "");
-        root.put("createusererror", "");
         userPromotion.setPercentOff(0.0);
         userPromotion.setCode("No Promotion Used");
 }
@@ -103,18 +96,15 @@ public class myservlet extends HttpServlet {
 			if (task.equals("none")){}
 			
 			else if (task.equals("CreateUser")){
-				template = "create.ftlh";
-				User potentialUser = null;
-				try{potentialUser = GetHandlers.makeUser(request);}
-				catch(Exception e) {e.printStackTrace();}
-				if(potentialUser != null) {
-					SendEmail sender = new SendEmail();
-					sender.actuallySendEmail(potentialUser, SendEmail.REGISTRATION_CONFIRMATION);
-					root.put("createusererror", "Created Successfully!!");
+				User potentialUser = GetHandlers.makeUser(request);
+				SendEmail sender = new SendEmail();
+				sender.actuallySendEmail(potentialUser, SendEmail.REGISTRATION_CONFIRMATION);
+				/*if(potentialUser != null) {
+	        	  		int i = UserDBManager.addUser(potentialUser);
 				}
 				else {
-					root.put("createusererror", GetHandlers.errorString);
-				}
+	        	  		System.out.println("REQUREMENTS NOT SATISFIED");
+				}*/
 			}//Create user
 			
 			else if (task.equals("SignIn")){
@@ -123,9 +113,13 @@ public class myservlet extends HttpServlet {
         	  		//CHECK THAT USER IS IN THE DATABASE
         	  		try {
         	  			User u = l.authorizeUser(userRequestingAuth);
-        	  			boolean userExists = false;
+         	  			boolean userExists = false;
         	  			if(u != null) userExists = true;
         	  			if(userExists) {
+               	  			ArrayList<Payment> pays = PaymentDBManager.searchPayment("uid", Integer.toString(u.getUid()));
+               	  			if (pays.size()>0){
+            	  			    pay = pays.get(pays.size()-1);
+               	  			}
         	  				thisUser = u;
         	  				if(thisUser.getType().equals("u")) {
         	  					accountdir = "useraccount";
@@ -143,10 +137,15 @@ public class myservlet extends HttpServlet {
         	  				else if(thisUser.getType().equals("m")) {
         	  					accountdir = "manageraccount";
         	  				}
+        					
+        					String cc = pay.getCc_number();
+        					cc = cc.substring(12, 16);
+        					Payment payment = new Payment();
+        					
         					root.put("user", thisUser);
+        					root.put("pay", pay);
+        					root.put("cc", cc);
         	  				template =  accountdir + "/account.ftlh";
-        	  				root.put("sadd", thisUser.getShipAddress());
-        	  				root.put("badd", thisUser.getBillAddress());
         					root.put("loginbutton", "Hello " + thisUser.getFname());
         	  			}
         	  		}
@@ -163,6 +162,10 @@ public class myservlet extends HttpServlet {
     	  			boolean userExists = false;
     	  			if(u != null) userExists = true;
     	  			if(userExists) {
+           	  			ArrayList<Payment> pays = PaymentDBManager.searchPayment("uid", Integer.toString(u.getUid()));
+           	  			if (pays.size()>0){
+        	  			    pay = pays.get(pays.size()-1);
+           	  			}
     	  				thisUser = u;
     	  				if(thisUser.getType().equals("u")) {
     	  					accountdir = "useraccount";
@@ -180,9 +183,14 @@ public class myservlet extends HttpServlet {
     	  				else if(thisUser.getType().equals("m")) {
     	  					accountdir = "manageraccount";
     	  				}
+
+    	  				String cc = pay.getCc_number();
+    	  				System.out.println(pay.getCcid());
+    					cc = cc.substring(12, 16);
+    					
     					root.put("user", thisUser);
-    	  				root.put("sadd", thisUser.getShipAddress());
-    	  				root.put("badd", thisUser.getBillAddress());
+    					root.put("pay", pay);
+    					root.put("cc", cc);
     					root.put("loginbutton", "Hello " + thisUser.getFname());
     	    	  		
     	  			}
@@ -689,7 +697,6 @@ public class myservlet extends HttpServlet {
 
 					Transaction t = GetHandlers.CreateTransaction(request, thisUser, userPromotion.getCode(), cart);
 					TransactionDBManager.addTransaction(t);
-     				CartDBManager.addCart(thisUser.getUid());
      				if (carts.size() > 1) {
 		        			cart = carts.get(carts.size() - 1);
 		        		}
